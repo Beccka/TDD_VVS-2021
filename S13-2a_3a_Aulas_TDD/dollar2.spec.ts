bed03e1ad2024b6b09e3019f63144943dc72d5c5
Rebeca Adamuz <rebeca.adamuz@gmail.com>
2021-07-27 22:26:16 -0300

    S13-2aAula

---
+++ S13-2a_3a_Aulas_TDD/dollar2.spec.ts
@@ -0,0 +1,11 @@
+import { Dollar } from './dollar2' 
+
+describe('Dollar', () => {
+    it('should return 10 when five dollars are multiplied by two', () => {
+        const five: Dollar = new Dollar(5);
+        var product: Dollar = five.times(2);
+        expect(product.amount).toEqual(10);
+        product = five.times(3);
+        expect(product.amount).toEqual(15);
+    })
+});
