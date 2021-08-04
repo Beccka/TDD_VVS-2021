4104f6628023c5a8e2d961c0b958a8071d1632ac
Rebeca Adamuz <rebeca.adamuz@gmail.com>
2021-08-03 22:31:24 -0300

    S14-6aAula

---
+++ S14-3a_a_9a_Aulas_TDD/money6.spec.ts
@@ -0,0 +1,23 @@
+import { Money} from './money6' 
+
+describe('Money', () => {
+    it('Should correctly handle dollar multiplication', () => {
+        const five: Money = Money.dollar(5);
+        expect(five.times(2).equals(Money.dollar(10))).toBeTruthy();
+        expect(five.times(3).equals(Money.dollar(15))).toBeTruthy();
+    })
+
+    it('Should correctly handle euro multiplication', () => {
+        const five: Money = Money.euro(5);
+        expect(five.times(2).equals(Money.euro(10))).toBeTruthy();
+        expect(five.times(3).equals(Money.euro(15))).toBeTruthy();
+    })
+
+    it('Should correctly handle money equality', ()=> {
+        expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
+        expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
+        expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy();
+        expect(Money.euro(5).equals(Money.euro(6))).toBeFalsy();
+        expect(Money.euro(5).equals(Money.dollar(5))).toBeFalsy();
+    })
+});
